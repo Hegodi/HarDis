f19364fa0c41aff45b57fd9c3338cde82db4426a
Diego Gonzalez <diegonher@gmail.com>
2017-11-12 20:55:20 +0100

    GPL message in all files

--- src/Particle.java
+++ src/Particle.java
@@ -1,3 +1,26 @@
+/*
+  This file is part of HARDIS.
+  
+  HARDIS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  HARDIS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Hardis 1.0.  If not, see <http://www.gnu.org/licenses/>.
+
+ ---------------------------------------------------------------------
+
+  Author: Diego Gonzalez.
+  Email: diegonher@gmail.com
+  November 2017
+
+*/
 public class Particle {
 
   public double rx;
